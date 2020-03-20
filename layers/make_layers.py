c2d9792c8c0f7f10b33d605be423815c84491355
Aiden-Jeon <ells2124@gmail.com>
2020-03-20 15:29:26 +0000

    change args dataset to input_size

--- layers/make_layers.py
+++ layers/make_layers.py
@@ -1,42 +1,44 @@
 from layers import ConvLayer, ConvTransposeLayer
 
 
-def make_generator_layers(dataset: str, latent_dim: int = 100, hidden_channel: int = 128) -> list:
-    if dataset in ["mnist", "fmnist"]:
+def make_generator_layers(input_size: int, latent_dim: int = 100, hidden_channel: int = 128, last_act: str = "sigmoid") -> list:
+    # mnist, fmnist
+    if input_size == 28:
         layers = [
             ConvTransposeLayer(latent_dim, hidden_channel * 4, 4, 1, 0, True, "relu"),
             ConvTransposeLayer(hidden_channel * 4, hidden_channel * 2, 4, 1, 0, True, "relu"),
             ConvTransposeLayer(hidden_channel * 2, hidden_channel, 4, 2, 1, True, "relu"),
-            ConvTransposeLayer(hidden_channel, 1, 4, 2, 1, False, "tanh"),
+            ConvTransposeLayer(hidden_channel, 1, 4, 2, 1, False, last_act),
         ]
-    elif dataset in ["lsun"]:
+    # cifar10
+    elif input_size == 32:
         layers = [
-            ConvTransposeLayer(latent_dim, hidden_channel * 8, 4, 1, 0, True, "relu"),
-            ConvTransposeLayer(hidden_channel * 8, hidden_channel * 4, 4, 2, 1, True, "relu"),
+            ConvTransposeLayer(latent_dim, hidden_channel * 4, 4, 1, 0, True, "relu"),
             ConvTransposeLayer(hidden_channel * 4, hidden_channel * 2, 4, 2, 1, True, "relu"),
             ConvTransposeLayer(hidden_channel * 2, hidden_channel, 4, 2, 1, True, "relu"),
-            ConvTransposeLayer(hidden_channel, 3, 4, 2, 1, False, "tanh"),
+            ConvTransposeLayer(hidden_channel, 3, 4, 2, 1, False, last_act),
         ]
     else:
         raise ValueError
     return layers
 
 
-def make_discriminator_layers(dataset: str, hidden_channel: int = 128) -> list:
-    if dataset in ["mnist", "fmnist"]:
+def make_discriminator_layers(input_size: int, hidden_channel: int = 128) -> list:
+    # mnist, fmnist
+    if input_size == 28:
         layers = [
             ConvLayer(1, hidden_channel, 4, 2, 1, True, "leakyrelu"),
             ConvLayer(hidden_channel, hidden_channel * 2, 4, 2, 1, True, "leakyrelu"),
-            ConvLayer(hidden_channel * 2, hidden_channel * 4, 4, 1, 0, True, "leakyrelu"),
-            ConvLayer(hidden_channel * 4, 1, 4, 1, 0, False, "sigmoid"),
+            ConvLayer(hidden_channel * 2, hidden_channel * 4, 4, 2, 1, True, "leakyrelu"),
+            ConvLayer(hidden_channel * 4, 1, 3, 1, 0, False, "sigmoid"),
         ]
-    elif dataset in ["lsun"]:
+    # cifar10
+    elif input_size == 32:
         layers = [
             ConvLayer(3, hidden_channel, 4, 2, 1, True, "leakyrelu"),
             ConvLayer(hidden_channel, hidden_channel * 2, 4, 2, 1, True, "leakyrelu"),
             ConvLayer(hidden_channel * 2, hidden_channel * 4, 4, 2, 1, True, "leakyrelu"),
-            ConvLayer(hidden_channel * 4, hidden_channel * 8, 4, 2, 1, True, "leakyrelu"),
-            ConvLayer(hidden_channel * 8, 1, 4, 1, 0, False, "sigmoid"),
+            ConvLayer(hidden_channel * 4, 1, 4, 1, 0, False, "sigmoid"),
         ]
     else:
         raise ValueError
